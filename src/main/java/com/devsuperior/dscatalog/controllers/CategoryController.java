5a40e8472aacedc4bf9ccca26378b2c3b4098f54
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-06-17 11:43:45 -0300

    CRUD Category, exception handling

--- src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -3,12 +3,13 @@ package com.devsuperior.dscatalog.controllers;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -17,9 +18,35 @@ public class CategoryController {
     @Autowired
     private CategoryService service;
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> result = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> result = service.findAll(pageable);
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@RequestBody CategoryDTO dto, @PathVariable Long id) {
+        dto = service.update(dto, id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,8 +3,15 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,8 +21,50 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<CategoryDTO> findAll() {
-        List<Category> result = repository.findAll();
-        return result.stream().map(c -> new CategoryDTO(c)).toList();
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        return new CategoryDTO(repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado.")));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CategoryDTO> findAll(Pageable pageable) {
+        Page<Category> result = repository.findAll(pageable);
+        return result.map(c -> new CategoryDTO(c));
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category c = new Category();
+        dtoToEntity(dto, c);
+        return new CategoryDTO(repository.save(c));
+    }
+
+    @Transactional
+    public CategoryDTO update(CategoryDTO dto, Long id) {
+        try {
+            Category c = repository.getReferenceById(id);
+            dtoToEntity(dto, c);
+            return new CategoryDTO(repository.save(c));
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void dtoToEntity(CategoryDTO dto, Category c) {
+        c.setName(dto.getName());
     }
 }
