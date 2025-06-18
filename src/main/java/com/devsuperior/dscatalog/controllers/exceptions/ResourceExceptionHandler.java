150b6c3aeb5c57a2d8f13ba30b3cc9f4f312de88
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-06-18 09:32:33 -0300

    Auditing data

--- src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.controllers.exceptions;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,13 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,9 +8,11 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -51,7 +53,7 @@ public class CategoryService {
         }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
         if (!repository.existsById(id)) {
             throw new ResourceNotFoundException("Recurso não encontrado");
@@ -59,7 +61,7 @@ public class CategoryService {
         try {
             repository.deleteById(id);
         }
-        catch (DataIntegrityViolationException e) {
+        catch (DataIntegrityViolationException | EmptyResultDataAccessException e) {
             throw new DatabaseException("Falha de integridade referencial");
         }
     }
