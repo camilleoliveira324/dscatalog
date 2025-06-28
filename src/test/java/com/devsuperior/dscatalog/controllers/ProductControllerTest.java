ca97777a9623900b132261e74f3f4edbce3756ac
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-06-28 10:26:18 -0300

    Controller testes delete, update e insert

--- src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTest.java
+++ src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTest.java
@@ -2,25 +2,26 @@ package com.devsuperior.dscatalog.controllers;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.util.Factory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +30,9 @@ public class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private long existingId;
     private long nonExistingId;
@@ -52,6 +56,75 @@ public class ProductControllerTest {
 
         when(service.findById(existingId)).thenReturn(dto);
         when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        when(service.update(any(), eq(existingId))).thenReturn(dto);
+        when(service.update(any(), eq(nonExistingId))).thenThrow(ResourceNotFoundException.class);
+
+        when(service.insert(any())).thenReturn(dto);
+
+        doNothing().when(service).delete(existingId);
+        doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+        doThrow(DatabaseException.class).when(service).delete(dependentId);
+
+    }
+
+    @Test
+    public void deleteShouldReturnNoContentWhenIdExistis() throws Exception {
+        mockMvc.perform(delete("/products/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void deleteShouldReturnNotFountWhenIdExistis() throws Exception {
+        mockMvc.perform(delete("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteShouldReturnBadRequestWhenIdExistis() throws Exception {
+        mockMvc.perform(delete("/products/{id}", dependentId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void insertShouldReturnProductDTO() throws Exception {
+        String json = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(post("/products")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExistis() throws Exception {
+        String json = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(put("/products/{id}", existingId)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists());
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExistis() throws Exception {
+        String json = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(put("/products/{id}", nonExistingId)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
 
--- src/test/java/com/devsuperior/dscatalog/util/Factory.java
+++ src/test/java/com/devsuperior/dscatalog/util/Factory.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.util;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
@@ -18,5 +19,11 @@ public class Factory {
         var p = new Product(1L, "Phone", Instant.parse("2020-07-13T20:50:07.12345Z"), 800.0, "Good phone","https://raw.githubusercontent.com/devsuperior");
         p.getCategories().add(new Category(2L, "Electronics"));
         return new ProductDTO(p);
+    }
+
+    public static ProductDTO createProductDTOWithoutId() {
+        var p = new ProductDTO(null, "Phone", Instant.parse("2020-07-13T20:50:07.12345Z"), 800.0, "Good phone","https://raw.githubusercontent.com/devsuperior");
+        p.getCategories().add(new CategoryDTO(2L, "Electronics"));
+        return p;
     }
 }
